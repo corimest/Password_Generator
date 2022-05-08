623dc62af45f4d12a2b7f0885ef39455eb0ffb69
Jane Eyre <corimest@gmail.com>
2022-05-08 18:34:38 -0500

    random math part. 1

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,5 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 const numbers = ["1234567890"];
 const lowers = ["abcdefghijklmnopqrstuvwxyz"]
@@ -17,12 +14,9 @@ function writePassword() {
 
       if (promptLength > 8 && promptLength < 128){
         window.alert("Thanks! We won't tell anyone.")
-        //tell program this is password length
       }
       else if (promptLength < 8 || promptLength > 128) {
         window.alert("Oops! Let's try again. Remember, your password must be between 8 and 128 characters.");
-
-        writePassword()
       }
 
 
@@ -30,11 +24,10 @@ function writePassword() {
     var promptLower = window.confirm("Would you like to use lowercase letters?")
       if (promptLower) {
         window.alert("Great! We will add some lowercase letters to your password!")
-        //store info to generate, move on to next prompt
+        password += lowers;
       }
       else if (!promptLower) {
         window.alert("No worries. We'll leave those out this time.")
-        //move to next prompt
       }
   
 
@@ -42,11 +35,10 @@ function writePassword() {
     var promptUpper = window.confirm("Would you like to use uppercase letters?")
       if (promptUpper) {
         window.alert("Alrighty, then!")
-        //store info to generate, move on
+        password += uppers
       }
       else if (!promptUpper) {
         window.alert("Ok. We never liked uppercase letters anyway.")
-        //move on 
       }
 
 
@@ -54,6 +46,7 @@ function writePassword() {
     var promptNumbers = window.confirm("Would you like to add some numbers in there?")
       if (promptNumbers) {
         window.alert("Yep, we thought so!")
+        password += numbers
       }
       else if (!promptNumbers){
         window.alert("We'll send them packing.")
@@ -64,11 +57,38 @@ function writePassword() {
     var promptSpecial = window.confirm("Want to spice things up with some special characters?")
       if (promptSpecial) {
         window.alert("This password is going to be too hot to handle!")
+        password += specials
       }
       else if (!promptSpecial) {
         window.alert("Too much spice gives us heartburn.")
       }
+
+function generatePassword() {
+
+  for (let i = 0; i<1 ; i++) {
+    if (promptLower){
+      password += lowers;
+    }
+    if (promptUpper){
+      password += uppers;
+    }
+    if (promptNumbers){
+      password += numbers;
+    }
+    if (promptSpecial){
+      password += specials;
+    }
+  }
+  return password;
+};
+
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+    
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
